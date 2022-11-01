baf7f4f93356ad5fcf2862bbd21f965f65885034
GunWooJung <51355208+GunWooJung@users.noreply.github.com>
2022-11-01 14:55:12 +0900

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -5,14 +5,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "inf_int.h"
 #include <chrono>
 
 class calculator {
 private:
+    static void showMenu();
     static int priority(std::string);           //Priority of operator
-    static bool verifyToken(std::string);       //Verifing whether token is vaild
-    static void divideByToken(const std::string& , std::vector<std::string>&);  //Divide input string with token
+    static bool verifyNumber(std::string);       //Verifing whether token is vaild
+    static bool verifyVariable(std::string);
+    static bool verifyOperator(std::string s);
+    static void divideByToken(const std::string& , std::vector<std::string>&); 
+    static void divideByTokenVariable(const std::string&, std::vector<std::string>&);//Divide input string with token
     static void covertToPostFix(const std::vector<std::string>& , std::vector<std::string>&); //Convert to PostFix notation
     static inf_int calculatePostFix(const std::vector<std::string>&);     //Calculating PostFix notation
 public:
