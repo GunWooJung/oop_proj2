a5d6e665dbf88f28a55a55e8500a6b1d5b5221d4
GunWooJung <51355208+GunWooJung@users.noreply.github.com>
2022-11-02 14:27:51 +0900

    Update calculator.h
    주석 정리해줬음

--- calculator.h
+++ calculator.h
@@ -11,13 +11,13 @@
 
 class calculator {
 private:
-    static void showMenu();
+    static void showMenu();                     //Show main menu
     static int priority(std::string);           //Priority of operator
-    static bool verifyNumber(std::string);       //Verifing whether token is vaild
-    static bool verifyVariable(std::string);
-    static bool verifyOperator(std::string s);
-    static void divideByToken(const std::string& , std::vector<std::string>&); 
-    static void divideByTokenVariable(const std::string&, std::vector<std::string>&);//Divide input string with token
+    static bool verifyNumber(std::string);       //Verifing whether token is number
+    static bool verifyVariable(std::string);    //Verifing whether token is variable
+    static bool verifyOperator(std::string s);  //Verifing whether token is operator
+    static void divideByToken(const std::string& , std::vector<std::string>&); //Divide token by spacebar(no variable)
+    static void divideByTokenVariable(const std::string&, std::vector<std::string>&);//Divide token by spacebar(contains variable)
     static void covertToPostFix(const std::vector<std::string>& , std::vector<std::string>&); //Convert to PostFix notation
     static inf_int calculatePostFix(const std::vector<std::string>&);     //Calculating PostFix notation
 public:
